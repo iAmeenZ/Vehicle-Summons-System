049ab80e85d5225f459d9186c2834852f614df31
Nur Ameerul Ameen <40099907+iAmeenZ@users.noreply.github.com>
2021-10-08 03:56:31 +0800

    Delete php directory

--- php/errors.php
+++
@@ -1,7 +0,0 @@
-<?php  if (count($errors) > 0) : ?>
-  <div class="error">
-  	<?php foreach ($errors as $error) : ?>
-  	  <p><li><?php echo $error ?></li></p>
-  	<?php endforeach ?>
-  </div>
-<?php  endif ?>
