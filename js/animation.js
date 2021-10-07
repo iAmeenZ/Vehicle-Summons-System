0acec6a4ffc3fd4a813f15096699b9025d65de5e
Nur Ameerul Ameen <40099907+iAmeenZ@users.noreply.github.com>
2021-10-08 03:56:26 +0800

    Delete js directory

--- js/animation.js
+++
@@ -1,9 +0,0 @@
-
-// PROFILE.HTML
-function editModal(type, session, inputType, variable) {
-	document.getElementById('edit-modal').style.display='block';
-	document.getElementById('editType').innerHTML = type;
-	document.getElementById('editSession').value = session;
-	document.getElementById('editSession').type = inputType;
-	document.getElementById('editSessionRadio').value = variable;
-}
